ea27675c1e899aad28bfb2c499798fe2f71f52e6
Mark Hendriksen <hendriksen-mark@hotmail.com>
2026-01-04 22:16:13 +0100

    Refactor handleRoot to separate header file
    Moved the handleRoot function from mesh.cpp to a new mesh_http_content.h header file for better code organization. Updated mesh.h to include the new header and removed the redundant declaration.

--- ESP32/ESP32_C3_Dimmable_i2c_h/include/mesh.h
+++ ESP32/ESP32_C3_Dimmable_i2c_h/include/mesh.h
@@ -10,13 +10,13 @@
 #include "config.h"
 #include "custom_log.h"
 #include "processCommand.h"
+#include "mesh_http_content.h"
 
 void mesh_setup();
 void mesh_loop();
 void send_change();
 void newConnectionCallback(uint32_t nodeId);
 void receivedCallback(uint32_t from, String &msg);
-void handleRoot();
 void sendData(String msg);
 void set_Target_Pos_test();
 void set_Target_Pos();
